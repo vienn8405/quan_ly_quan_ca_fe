b7d012b8cfff2032c8cd34e41ad926c5e7a48b23
vienn8405 <vienn8405@gmail.com>
2026-02-03 09:44:32 +0700

    Configure PostgreSQL database

--- quan_ly_quan_ca_fe/settings.py
+++ quan_ly_quan_ca_fe/settings.py
@@ -73,10 +73,21 @@ WSGI_APPLICATION = 'quan_ly_quan_ca_fe.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+#DATABASES = {
+ #   'default': {
+  #      'ENGINE': 'django.db.backends.sqlite3',
+   #     'NAME': BASE_DIR / 'db.sqlite3',
+    #}
+#}
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'quan_ly_quan_ca_fe',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres123',   
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
